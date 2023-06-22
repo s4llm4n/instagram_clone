fecde2e89b03e7441029ff0098e4fb827a62cbd4
s4llm4n <s4llm4n@gmail.com>
2023-06-22 20:38:58 +0700

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -9,7 +10,19 @@ import 'package:instagram_clone/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyBjfEYd_C9IMvGPpG-65GMl_0WpW__fBwQ', 
+        appId: '1:330199817353:web:610ec2997b3053a8317a58', 
+        messagingSenderId: '330199817353', 
+        projectId: 'instagram-tut-e3474',
+        storageBucket: 'instagram-tut-e3474.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
