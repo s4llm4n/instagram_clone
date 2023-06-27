593c4fdf16bbdfc04cfc0abeac9177973c5f6e40
s4llm4n <s4llm4n@gmail.com>
2023-06-27 23:24:40 +0700

    changedimension

--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/utils/dimension.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/utils/global_varaibles.dart';
+import 'package:provider/provider.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+class ResponsiveLayout extends StatefulWidget {
   final Widget webScreeenLayout;
   final Widget mobileScreenLayout;
   const ResponsiveLayout({ Key? key,
@@ -10,13 +12,30 @@ class ResponsiveLayout extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  addData() async {
+    UserProvider _userProvider = Provider.of(context, listen: false);
+    await _userProvider.refreshUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constrains) {
         if(constrains.maxWidth > webScreenSize) {
-          return webScreeenLayout;
+          return widget.webScreeenLayout;
         }
-        return mobileScreenLayout;
+        return widget.mobileScreenLayout;
       },
     );
   }
